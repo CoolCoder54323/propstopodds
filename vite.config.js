1159edcfaccf8c58b81a95748f21e43f5152c487
nicholasdixon <nicxellent@icloud.com>
2024-01-14 06:51:52 -0500

    added public dir

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js}']
+	},
+	kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
 	}
+	
 });
